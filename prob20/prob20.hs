3c57eee7afe4dd8092055b009cc60264da592870
Kyrill Tkachov <kyrilltkachov@yandex.ru>
2011-03-26 15:20:15 +0000

    added two problems

---
+++ prob20/prob20.hs
@@ -0,0 +1,21 @@
+{-
+n! means n  (n  1)  ...  3  2  1
+
+For example, 10! = 10  9  ...  3  2  1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+-}
+
+main = print f
+
+f = sum $ digits $ fact 100
+
+facs :: [Integer]
+facs = scanl (*) 1 [1..]
+
+fact :: Int -> Integer
+fact n = facs !! n
+
+digits :: Integer -> [Int]
+digits n = map read $ map return (show n)
