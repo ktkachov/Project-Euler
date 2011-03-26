ada7911672ad842eb4b8c549e9befa8d9ae67a7c
Kyrill Tkachov <kyrilltkachov@yandex.ru>
2011-03-26 01:55:59 +0000

    reorganised structure a bit and started adding comments

--- prob1.hs
+++
@@ -1,2 +0,0 @@
-f :: Int
-f = sum $ filter (\x -> x `mod` 3 == 0 || x `mod` 5 == 0) [1..999]
--- prob2.hs
+++
@@ -1,5 +0,0 @@
-fibs :: [Integer]
-fibs = scanl (+) 0 (1:fibs)
-
-f :: Integer
-f = sum $ filter even $ takeWhile (<4000000) fibs
