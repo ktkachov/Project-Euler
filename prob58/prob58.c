58e909bc68e666de8de759efc32c236a7338df82
Kyrill Tkachov <kyrilltkachov@yandex.ru>
2011-03-30 23:51:03 +0300

    updated prob58 to not use sieve of Eratosthenes
    It's too slow for large primes

--- prob58/prob58.c
+++ prob58/prob58.c
@@ -1,40 +1,34 @@
 #include <stdio.h>
 
 
-int main(int argc, char* argv[]) {
+inline int isPrime(unsigned long long n) {
+  if (n % 2 == 0) {
+    return 0;
+  }
+  float sqrtn = sqrt(n);
+  unsigned long long i = 3;
+  while (i <= sqrtn) {
+    if (n % i == 0) {
+      return 0;
+    }
+    i += 2;
+  }
+  return 1;
+}
 
-  unsigned long long nPrimes = 1000000000;
-  unsigned long long i = 0;
+int main(int argc, char* argv[]) {
   unsigned long long step = 2;
   unsigned long long size = 1;
   float ratio = 1.0;
   unsigned long long num = 1;
   unsigned long long numPrimes = 0;
   unsigned long long numElems = 1;
-  short* candidates = (short*)calloc(nPrimes, sizeof(short));
-  if (!candidates) {
-    printf("failed to allocate memory\n");
-    exit(-1);
-  }
-  /*
-    Perform sieve of Eratosthenes to filter the primes
-  */
-  unsigned long long p = 2;
-  while (p*p < nPrimes) {
-    unsigned long long k = 0;
-    for (k = p * p; k < nPrimes; k+=p) {
-      candidates[k] = 1;
-    }
-    while (candidates[++p] != 0)
-     ;
-  }
-
   while (ratio >= 0.1) {
     int j = 0;
     for (j = 0; j < 4; ++j) {
       num += step;
       ++numElems;
-      if (!candidates[num]) {
+      if (isPrime(num)) {
         ++numPrimes;
       }
     }
@@ -42,7 +36,6 @@ int main(int argc, char* argv[]) {
     step += 2;
     ratio = (float) numPrimes / (float) numElems;
   }
-  free(candidates);
   printf("%lld\n", size);
   return 0;
 }
